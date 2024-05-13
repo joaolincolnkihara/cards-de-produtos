11b99c7b7fb59ef14c347173f9d0c073c6c92f16
João Lincoln <157829347+joaolincolnkihara@users.noreply.github.com>
2024-05-13 12:22:29 -0300

    update cores

--- cards/script/script.js
+++ cards/script/script.js
@@ -1,5 +1,14 @@
-let cor2 = document.
-
 function clicou(){
-    
+    let img2=document.getElementById('img-2')
+    let img22= document.getElementById('img-22')
+
+    // img22.style.display='block'
+    // img2.style.display='none'
+    if(img2.style.display == 'block'){
+        img22.style.display = 'block'
+        img2.style.display = 'none'
+    }else{
+        img2.style.display = 'none'
+        img22.style.display= 'block'
+    }
 }
