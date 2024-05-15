495fa0bd95139a9e1c2bf88ad77366d8fd9576f7
João Lincoln <157829347+joaolincolnkihara@users.noreply.github.com>
2024-05-15 11:31:41 -0300

    nome das cores

--- cards/script/script.js
+++ cards/script/script.js
@@ -4,25 +4,35 @@
     let img4 = document.getElementById('img-4')
     let img5 = document.getElementById('img-5')
 
+    let msg = document.getElementById('msg')
+
 function CelularCorPreta(){
+    msg.innerHTML = 'Preto'
+
     img2.style.display='block'
     img3.style.display='none'
     img4.style.display='none'
     img5.style.display='none'
 }
 function CelularCorVerde(){
+    msg.innerHTML = 'Verde'
+
     img2.style.display='none'
     img3.style.display='block'
     img4.style.display='none'
     img5.style.display='none'
 }
 function CelularCorCreme(){
+    msg.innerHTML = 'Creme'
+
     img2.style.display='none'
     img3.style.display='none'
     img4.style.display='block'
     img5.style.display='none'
 }
 function CelularCorVioleta(){
+    msg.innerHTML = 'Violeta'
+
     img2.style.display='none'
     img3.style.display='none'
     img4.style.display='none'
