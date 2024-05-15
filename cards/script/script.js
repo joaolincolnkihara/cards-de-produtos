3dda47845063001728b7df87167a5e5b5729eaac
João Lincoln <157829347+joaolincolnkihara@users.noreply.github.com>
2024-05-15 08:55:59 -0300

    +script mudança de cor celular

--- cards/script/script.js
+++ cards/script/script.js
@@ -1,14 +1,30 @@
-function clicou(){
-    let img2=document.getElementById('img-2')
-    let img22= document.getElementById('img-22')
+// COR CELULAR
+    let img2 = document.getElementById('img-2')
+    let img3 = document.getElementById('img-3')
+    let img4 = document.getElementById('img-4')
+    let img5 = document.getElementById('img-5')
 
-    // img22.style.display='block'
-    // img2.style.display='none'
-    if(img2.style.display == 'block'){
-        img22.style.display = 'block'
-        img2.style.display = 'none'
-    }else{
-        img2.style.display = 'none'
-        img22.style.display= 'block'
-    }
+function CelularCorPreta(){
+    img2.style.display='block'
+    img3.style.display='none'
+    img4.style.display='none'
+    img5.style.display='none'
+}
+function CelularCorVerde(){
+    img2.style.display='none'
+    img3.style.display='block'
+    img4.style.display='none'
+    img5.style.display='none'
+}
+function CelularCorCreme(){
+    img2.style.display='none'
+    img3.style.display='none'
+    img4.style.display='block'
+    img5.style.display='none'
+}
+function CelularCorVioleta(){
+    img2.style.display='none'
+    img3.style.display='none'
+    img4.style.display='none'
+    img5.style.display='block'
 }
